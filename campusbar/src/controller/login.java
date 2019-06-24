e79afe16c2770629e5dfb91db6c68e90e874ea06
vincenzo <vincenzo@vincenzo-EZbook>
2019-06-24 11:54:09 +0200

    login

--- campusbar/src/controller/login.java
+++ campusbar/src/controller/login.java
@@ -19,7 +19,7 @@ public class login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("saverio gay");
 	}
 
 	/**
