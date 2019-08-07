a6c065110ba6c887605e11b021ac57108f463702
vincenzo <vincenzo@vincenzo-EZbook>
2019-08-07 18:37:57 +0200

    registrazione

--- campusbar/src/controller/registrazione.java
+++ campusbar/src/controller/registrazione.java
@@ -8,25 +8,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class login
+ * Servlet implementation class registrazione
  */
-@WebServlet("/login")
-public class login extends HttpServlet {
-	
+@WebServlet("/registrazione")
+public class registrazione extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public registrazione() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
-		System.out.println("ciao a tutti e benvenuti!!!");
+		System.out.println("Registrazione ok/11");
 	}
 
 	/**
