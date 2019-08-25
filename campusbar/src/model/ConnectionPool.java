8f772537c8348affffbe20c4a734d481ec5db403
vincenzo <vincenzo@vincenzo-EZbook>
2019-08-25 19:27:52 +0200

    connectionpool

--- campusbar/src/model/ConnectionPool.java
+++ campusbar/src/model/ConnectionPool.java
@@ -17,10 +17,10 @@ public class ConnectionPool {
 			p.setDriverClassName("com.mysql.cj.jdbc.Driver");
 			p.setUsername("root");
 			p.setPassword("sasa");
-			p.setMaxActive(100);
+			p.setMaxActive(500);
 			p.setInitialSize(10);
 			p.setMinIdle(10);
-			p.setRemoveAbandonedTimeout(60);
+			p.setRemoveAbandonedTimeout(120);
 			p.setRemoveAbandoned(true);
 			datasource = new DataSource();
 			datasource.setPoolProperties(p);
